b731c317006fa3efb7c248cc376a69abccc8e18c
durian <huyk25sinh@gmail.com>
2023-03-08 01:45:18 +0700

    detail

--- Learn-go-by-building-11-projects/Movies-CRUD/main.go
+++ Learn-go-by-building-11-projects/Movies-CRUD/main.go
@@ -11,10 +11,19 @@ import (
 )
 
 type Movie struct {
-	ID string `json:"ID"`
-	
+	ID string `json:"ID"` // json:"ID" is struct tag
+	Isbn string `json: "isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director`
 }
 
 type Director {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
 
+var movies []Movie
+
+func main(){
+	r:=mux.NewRouter()
 }
