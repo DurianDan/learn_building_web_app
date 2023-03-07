2e993ddc37effeb85c93859fc4778cfcfb118370
durian <huyk25sinh@gmail.com>
2023-03-07 16:41:48 +0700

    for loops in go

--- Golang-beginner-tutorial/main.go
+++ Golang-beginner-tutorial/main.go
@@ -20,36 +20,52 @@ func main(){
 	fmt.Printf("%v tickets left",remainingTickets)
 	fmt.Println("Get your ticket, please!")
 	
-	// arrays of size 50, and data type of strings
-	var bookings = [50]string{"Nana", "Nicole","Hannah"}
-	bookings[0] = "Dan"
-
-	// interact with the user
-	var firstName string
-	var lastName string
-	var userTickets int32
-	// var userTickets int16
-
-	//ask user for their name
-	// fmt.Scan() interpret spaces as seperators, seperate arguments 
-	// cant use fmt.Scan() with spaces
-	// use a pointer
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
-
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
-
-	fmt.Printf("Thank you, %v %v for booking %v tickets\n",
+	for {
+		// interact with the user
+		var firstName string
+		var lastName string
+		var userTickets int32
+		// var userTickets int16
+		
+		// ask user for their name
+		// fmt.Scan() interpret spaces as seperators, seperate arguments 
+		// cant use fmt.Scan() with spaces
+		// use a pointer
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
+		
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
+		
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
+		
+		fmt.Printf("Thank you, %v %v for booking %v tickets\n",
 		firstName, lastName, userTickets)
 
-	// Application Login
-	// math operation with the same type
-	remainingTickets = remainingTickets - int16(userTickets)
-	
-	fmt.Printf("%v tickets left\n", remainingTickets)
+		// arrays of size 50, and data type of string
+		// array's max size is fixed
+		// bookings can also be initialized by
+		// var bookings = [50]string{"Dan","Huy"} 
+		var bookings [50]string
+		bookings[0] = firstName + " " + lastName // asign data to the first element
+		
+		// slices are like lists in python
+		// dynamic size, 
+		// and can add element without calling the actual index
+		var bookings_list[]string
+		// append() append new element to a slice
+		// and return a new slice
+		bookings_list = append(bookings_list, firstName+ " "+lastName) 
+		fmt.Printf("%T\n",booking_list)
 
+		// Application Login
+		// math operation with the same type
+		remainingTickets = remainingTickets - int16(userTickets)
+		
+		firstName := []string{}
+		for 
+		
+		fmt.Printf("%v tickets left\n", remainingTickets)
+	}
 }
