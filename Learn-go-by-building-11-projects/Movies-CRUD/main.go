a32d08deed50a003c858ce802dd6f2ee88dd9b89
DurianDan <huyk25sinh@gmail.com>
2023-03-10 13:39:26 +0700

    makinf own function

--- Learn-go-by-building-11-projects/Movies-CRUD/main.go
+++ Learn-go-by-building-11-projects/Movies-CRUD/main.go
@@ -34,13 +34,76 @@ func getMovies(w http.ResponseWriter, r *http.Request){
 	// structs can be converted into json
 }
 
-func deleteMovies(w http.ResponseWriter, r *http.Request){
+func deleteMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r) // get params from request
+	for idx, item := range movies {
+		// ragne return the index and a copy of each value in a slice/array
+		// each item is of struct "Movie"
+		if item.ID == params["id"] {
+			movies = append(movies[:idx], movies[idx+1:]...)
+			// because movies[idx+1:] contains many elements
+			// append() can only recieve a slice, and element, not something containing multiple elements 
+			// in need to add ... to the end of it, to unwrap it into multiplt elements 
+			break
+			// this method can not work if in need of deleting multiple movies
+		}
+	}
+
+	// return the remainning movies
+	json.NewEncoder(w).Encode(movies)
 }
 
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	get_id := mux.Vars(r)["id"]
+	for _, item := range movies {
+		if item.ID == get_id {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
 
+func createMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	var new_movie = Movie{
+		params["ID"],
+		params["isbn"],
+		params["title"],
+		&Director{
+			params["firstname"],
+			params["lasstname"],
+		},
+	}
+	movies = append(movies, new_movie)
+	json.NewEncoder(w).Encode(new_movie)
+}
 
-func main(){
+func updateMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	var update_idx int
+	for idx, item := range movies {
+		if item.ID == params["ID"]{
+			update_idx = idx
+			movies[idx] = Movie{
+				params["ID"],
+				params["isbn"],
+				params["title"],
+				&Director{
+					params["firstname"],
+					params["lastname"],
+				},
+			}
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies[update_idx])
+}
+
+func main() {
 	r:=mux.NewRouter()
 
 	movies = append(movies,
@@ -57,7 +120,7 @@ func main(){
 	r.HandleFunc("/movies/id", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
-	r.HandleFunc("/movies/{id}",delteMovies).Methods("DELETE")
+	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
 
 	fmt.Println("Starting server at the port 8000: localhost:8000")
 
