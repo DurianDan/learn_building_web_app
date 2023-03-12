1cdaabe3cb04c8c3d292a3d553e12d24c6f622c9
durian <huyk25sinh@gmail.com>
2023-03-13 00:39:34 +0700

    learn about interface in go

--- Learn-go-by-building-11-projects/MySQL-CRUD-Book_management/pkg/config/app.go
+++ Learn-go-by-building-11-projects/MySQL-CRUD-Book_management/pkg/config/app.go
@@ -16,10 +16,16 @@ func Connect(){
 	// this function help the app to connect to the database 
 	d, err := gorm.Open( // The DB will return back the error to the var err, and value to var d
 		"mysql", // choosing the find of SQL database, (can also be PostGre, SQLite)
-		"root:mysql/simplerest", // {user-name}:{password}/{table-name}
+		"root:mysql/simplerest?charset=utf8&parseTime=True&loc=Local", 
+		// {user-name}:{password}/{table-name}?{MySQL specification}
 	)
 	if err!= nil {
 		panic(err) // check if the err has been returned (not nil)
 	}
 	db = d 
+}
+
+func GetDB() *gorm.DB{
+	// return the db from the database
+	return db // to talk to the database 
 }
